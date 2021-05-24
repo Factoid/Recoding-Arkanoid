c832b57f80c2f95d0d5f0565b745fe45e1cbaa34
Adrian Cheater <adrian@complexgames.com>
2021-05-23 21:10:01 -0500

    Between episode physics corrections

--- Assets/DestroyOnCollision.cs
+++ Assets/DestroyOnCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    private void OnCollisionEnter2D( Collision2D collision )
+    private void OnCollisionExit2D( Collision2D collision )
     {
         Destroy(gameObject);
     }
